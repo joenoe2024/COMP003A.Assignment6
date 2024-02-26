a43e5ae9d6d549b45af4d6fcb6b7c0fa778e2b2a
jojos <jojos@JOSEPH>
2024-02-25 18:17:20 -0800

    Add project files.

---
+++ COMP003A.Assignment6/Truck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Assignment6
+{
+    ///<summary> 
+    // Trucks extends the Vehicile class
+    // </summary>
+    internal class Truck : Vehicle
+    {
+        /****** fields section***/
+        private double _sizeOfTruckBed;
+        /****** contructors section ****/
+        ///<summary>
+        ///Constructor that takes 4 parameters: truckBrand, truckModel, truckNumberofWheels, and sizeoftruckBed
+        ///it resuses the base class (vehicle) constructor that takes 3 parameters: brand, model, numberofwheels
+        ///</summary>
+        ///<param name="truckBrand"></param>
+        ///<param name="truckModel"></param>
+        ///<param name="truckNumberOfWheels"></param>
+        ///<param name="sizeOfTruckBed"></param>
+        public Truck(string truckBrand, string truckModel, int truckNumberOfWheels, double sizeOfTruckBed) : base(truckBrand, truckModel, truckNumberOfWheels)
+        {
+            sizeOfTruckBed = sizeOfTruckBed;
+        }
+        /****** propertues section ******/
+
+        public double SizeOfTruckBed
+        {
+            get { return _sizeOfTruckBed; }
+            set { _sizeOfTruckBed = value; }
+        }
+
+        /****** methods section ****/
+        ///<summary>
+        ///Overides the base Vehicles GetInfo() but adds on to it. 
+        ///</summary>
+        public override void GetInfo()
+        {
+            Console.WriteLine($"I'm a {nameof(Truck)}.");
+            base.GetInfo();
+            Console.WriteLine($"I also have a {SizeOfTruckBed} ft truck bed");
+        }
+
+    }
+}
---
+++ Ivehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+    ///<summary>
+    ///Interfaces are contracts that the inheriting class has to implement.
+    ///Read https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/abstractions-abstract-types-and-interfaces
+    ///</summary>
+    iternal interface IVehicle
+    {
+        ///<summary>
+        ///this method is required to be implemendted in the inheriting class
+        ///</summary>
+        public void GetInfo();
+    }
+}
